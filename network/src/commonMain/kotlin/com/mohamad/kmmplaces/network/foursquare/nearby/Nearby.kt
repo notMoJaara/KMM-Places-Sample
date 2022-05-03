4c3e5218ee4a6eeecd72602d08269a1c98cac5eb
Mohamad Jaara <jaara.moh@gmail.com>
2022-05-04 01:13:35 +0200

    use the io dispatcher on the network call

--- network/src/commonMain/kotlin/com/mohamad/kmmplaces/network/foursquare/nearby/Nearby.kt
+++ network/src/commonMain/kotlin/com/mohamad/kmmplaces/network/foursquare/nearby/Nearby.kt
@@ -3,8 +3,10 @@ package com.mohamad.kmmplaces.network.foursquare.nearby
 import com.mohamad.kmmplaces.network.foursquare.FoursquareResponse
 import com.mohamad.kmmplaces.network.foursquare.model.PlacesListDTO
 import com.mohamad.kmmplaces.network.foursquare.wrapFoursquareRequest
+import com.mohamad.kmmplaces.util.KmmDispatcher
 import io.ktor.client.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.withContext
 
 interface Nearby {
     suspend fun findNearbyPlaces(param: NearbyParam?): FoursquareResponse<PlacesListDTO>
@@ -27,19 +29,23 @@ data class Location(
 }
 
 
-class NearbyImpl(private val httpClient: HttpClient) : Nearby {
+class NearbyImpl(
+    private val httpClient: HttpClient,
+    private val dispatcher: KmmDispatcher
+) : Nearby {
     override suspend fun findNearbyPlaces(param: Nearby.NearbyParam?): FoursquareResponse<PlacesListDTO> = wrapFoursquareRequest {
-        // TODO: add proper support for api version
-        httpClient.get("v3/$PLACES_PATH/$NEARBY_PATH") {
-            param?.apply {
-                location?.let { parameter(LOCATION_QUERY, it.toQueryParam) }
-                hacc?.let { parameter(HACC_QUERY, it) }
-                altitude?.let { parameter(ALTITUDE_QUERY, it) }
-                query?.let { parameter(QUERY_QUERY, it) }
-                limit?.let { parameter(LIMIT_QUERY, it) }
+        withContext(dispatcher.io) {
+            // TODO: add proper support for api version
+            httpClient.get("v3/$PLACES_PATH/$NEARBY_PATH") {
+                param?.apply {
+                    location?.let { parameter(LOCATION_QUERY, it.toQueryParam) }
+                    hacc?.let { parameter(HACC_QUERY, it) }
+                    altitude?.let { parameter(ALTITUDE_QUERY, it) }
+                    query?.let { parameter(QUERY_QUERY, it) }
+                    limit?.let { parameter(LIMIT_QUERY, it) }
+                }
             }
         }
-
     }
 
     private companion object {
